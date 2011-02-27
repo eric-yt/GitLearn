1fd93e799720e12a97bb3d95ac565f813a4ba4f3
Eric.Yang <eric-yt@hotmail.com>
2011-02-27 00:09:45 -0800

    branch commit

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include<studio.h>
 int main()
 {
-printf("version: 0001");
+time_t mytime;
+struct tm *mylocaltime;
+mytime=time(NULL);
+mylocaltime=localtime(&mytime);
+printf("Year:%d\n",mylocaltime->tm_year+1900);
+printf("version: 0001\n");
 printf("hello world!\n");
 return 0;
 }
